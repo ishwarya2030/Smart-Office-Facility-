4c842989682608a72338463d772a3851b4408806
ishwarya2030 <ishwaryai630@gmail.com>
2025-10-01 13:28:14 +0530

    Added folders and Java files

--- src/com/smartoffice/patterns/command/ICommand.java
+++
@@ -1,8 +0,0 @@
-package com.smartoffice.patterns.command;
-
-// DESIGN PATTERN: Command (Command Interface)
-// Declares a method for executing a particular action.
-// This decouples the object that invokes the operation from the one that knows how to perform it.
-public interface ICommand {
-    void execute();
-}
--- src/com/smartoffice/patterns/observer/ISubject.java
+++
@@ -1,9 +0,0 @@
-package com.smartoffice.patterns.observer;
-
-// DESIGN PATTERN: Observer (Subject Interface)
-// Defines the contract for any object that can be observed.
-public interface ISubject {
-    void addObserver(IObserver observer);
-    void removeObserver(IObserver observer);
-    void notifyObservers();
-}
